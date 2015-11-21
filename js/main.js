65a267a950faebc386b3123c6c151c4df34f07c5
Dylan_Macnab <dylanmacnab@gmail.com>
2015-11-21 00:11:45 -0800

    Add JS from codepen pen

--- js/main.js
+++ js/main.js
@@ -1,123 +1,107 @@
-// Create a countdown timer for work sessions and to remind you to get up and walk around
+// Building a Calculator
 
-var secondsRemaining;
-var timerInterval;
-var stateSetting;
-var paused = false;
+// Variables
+// ------------------------------------------------------
+
+var inputString = "";
+var result = 0;
+var display = document.getElementById("display");
 
 
-function resetPage() {
-  // reset background images
-  stateSetting.classList.remove("state-rotate");
-  // reset minutes input with placeholder text
-  document.getElementById("minutes").value = "";
+
+// Math Functions
+// -------------------------------------------------------
+
+function add(a, b) {
+  return (a + b);
 }
 
-function startBreak() {
+function multiply(a, b) {
+  return (a * b);
+}
 
-  // play sound when time is up!
-  var audio = document.getElementsByTagName("audio")[0];
-  audio.play();
-  // rotate state setting
-  stateSetting = document.getElementById("background-setting");
-  stateSetting.classList.add("state-rotate");
+function subtract(a, b) {
+  return (a - b);
+}
 
-  document.getElementById("minutes").value = "Enjoy your break";
-  setTimeout(resetPage, 5000);
+function divide(a, b) {
+  return (a / b);
 }
 
 
-function tick() {
-  var timeDisplay = document.getElementById("time-display");
+function calculateStuff(calcInput) {
 
-  // convert seconds into MM:SS
-  var min = Math.floor(secondsRemaining / 60);
-  var sec = secondsRemaining - (min * 60);
+  var inputArray = calcInput.split(/\+|-|\*|\//);
+  var num1 = parseInt(inputArray[0]);
+  var operation = calcInput.match(/\+|-|\*|\//);
+  var num2 = parseInt(inputArray[1]);
 
-  if (min < 10) {
-    min = "0" + min;
+  switch (operation[0]) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    default:
+		  console.log("something isn't going right!");
   }
+  display.innerHTML = result;
+}
 
-
-  // Check to see if seconds is less than 10
-  if (sec < 10) {
-    sec = "0" + sec;
-  }
-  // Concatenate MM:SS time display
-  var message = min + ":" + sec;
-  // set timer to display message string
-  timeDisplay.innerHTML = message;
-
-  // stop timer interval function when it reaches 0
-  if (secondsRemaining === 0) {
-    clearInterval(timerInterval);
-    startBreak();
-  }
-  // reduce 1 second with each interval
-  secondsRemaining--;
+function displayButtonInput(buttonInput) {
+  display.innerHTML += buttonInput;
 }
 
 
-function startTimer() {
-  var minutes = document.getElementById("minutes").value;
 
-  // convert minutes to seconds
-  secondsRemaining = minutes * 60;
+// EVENTS //
+// ------------------------------------------------------------------
 
-  // validate minutes input
-  if (secondsRemaining < 0 || isNaN(minutes) || minutes === "") {
-    document.getElementById("minutes").value = "";
-    document.getElementById("time-display").innerHTML = "00:00";
-    clearInterval(timerInterval);
-    return;
-  }
+// Seperate events for:
+  // number btns
+  // mathmatical operators
+  // equal sign btn
+  // and cancel btn
 
-  // clear interval if already in progress
-  clearInterval(timerInterval);
-  // set interval for each second of countdown
-  timerInterval = setInterval(tick, 1000);
-
+// Get buttons and add to input string when clicked
+var btnNumber = document.getElementsByClassName("btn-number");
+for (var i = 0; i < btnNumber.length; i++) {
+  btnNumber[i].addEventListener("click", function() {
+    inputString += (this.innerHTML);
+    displayButtonInput(this.innerHTML);
+  });
 }
 
-function pauseTimer() {
-  if (secondsRemaining > 0) {
-    if (paused === false) {
-      paused = true;
-      this.value = "Resume";
-      clearInterval(timerInterval);
-    } else {
-      paused = false;
-      this.value = "Pause";
-      timerInterval = setInterval(tick, 1000);
-    }
-  }
+var btnOperator = document.getElementsByClassName("btn-operator");
+for (var i = 0; i < btnOperator.length; i++) {
+  btnOperator[i].addEventListener("click", function() {
+    inputString += (this.innerHTML);
+    displayButtonInput(this.innerHTML);
+    console.log(inputString);
+  });
 }
 
-function resetTimer() {
-  clearInterval(timerInterval);
-  document.getElementById("minutes").value = "";
-  document.getElementById("time-display").innerHTML = "00:00";
-}
+var btnEquals = document.getElementById("btn-equals");
+btnEquals.addEventListener("click", function() {
+  calculateStuff(inputString);
+  inputString = "=";
+});
 
-// run code once window has loaded
-window.onload = function () {
-
-  // Event Listeners for Timer Controls
-  var startButton = document.getElementById("start");
-  startButton.addEventListener("click", startTimer);
-
-  var stopButton = document.getElementById('stop');
-  stopButton.addEventListener("click", pauseTimer);
-
-  var resetButton = document.getElementById('reset');
-  resetButton.addEventListener("click", resetTimer);
-
-};
+var btnCancel = document.getElementById("btn-cancel");
+btnCancel.addEventListener("click", function() {
+  inputString = "";
+  display.innerHTML = "";
+});
 
 
-
-// Bugs
-// Nan:Nan issue when clicking start timer button multiple times..
-// add pause and reset buttons
-// add sound
-// fade value in input when starting timer
+// Equals buttons should reset variables for next calculation
+// Fix colors
+// Add equation
+// Fix for loop to add event listners
